c2dc96c11ef77048e3ad7b9ce65c014046b2ad2f
Rémi <ponche62880@gmail.com>
2023-01-24 15:01:21 +0100

    feat: modification de la route accueil

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -8,11 +8,11 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class AccueilController extends AbstractController
 {
-    #[Route('/accueil', name: 'app_accueil')]
+    #[Route('/', name: 'app_accueil')]
     public function index(): Response
     {
         return $this->render('accueil/index.html.twig', [
-            'controller_name' => 'Bibliothque',
+            'controller_name' => 'Bibliotheque',
         ]);
     }
 
