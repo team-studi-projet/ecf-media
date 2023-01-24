66a56420061f5505ae38b72be7067c0eb84d8429
Rémi <ponche62880@gmail.com>
2023-01-24 14:39:07 +0100

    feat: création d'un premier controlleur

---
+++ src/Controller/AccueilController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AccueilController extends AbstractController
+{
+    #[Route('/accueil', name: 'app_accueil')]
+    public function index(): Response
+    {
+        return $this->render('accueil/index.html.twig', [
+            'controller_name' => 'Bibliothque',
+        ]);
+    }
+
+    #[Route('/contact', name: 'app_contact')]
+    public function contact(): Response
+    {
+        return $this->render('contact/index.html.twig'); 
+    }
+}
